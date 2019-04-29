f7ac16f01c944cebb571a5d5db9fcd001500fa36
209420 <209420@edu.p.lodz.pl>
2019-04-29 13:50:42 +0200

    Dodanie wyświetlania gitID

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    printText("Kicajace-Zajace Developer rwitczyk");
+	    printText("bPapuzinski");
     }
 
     public static void printText(String str){
