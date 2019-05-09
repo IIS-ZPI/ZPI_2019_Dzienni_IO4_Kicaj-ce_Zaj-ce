e9c5fc48e4612ccba011048e9be3ca6fe461c6cb
209420 <209420@edu.p.lodz.pl>
2019-05-09 17:42:45 +0200

    zad7
    implementacja odejmowanka
    
    (cherry picked from commit 2428b334a9915e62c811162cd501dc47cbaaeeeb)

--- src/MathOper.java
+++ src/MathOper.java
@@ -1,4 +1,4 @@
-public class MathOper implements IArithemicsAdd, IArithmeticsDiv {
+public class MathOper implements IArithemicsAdd, IArithmeticsDiff, IArithmeticsDiv  {
     @Override
     public double Addition(double A, double B) {
         return A + B;
@@ -7,5 +7,10 @@ public class MathOper implements IArithemicsAdd, IArithmeticsDiv {
     @Override
     public double Devision(double A, double B) {
         return A / B;
+    }
+
+    @Override
+    public double Difference(double A, double B) {
+        return A - B;
     }
 }
