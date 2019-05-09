2428b334a9915e62c811162cd501dc47cbaaeeeb
209420 <209420@edu.p.lodz.pl>
2019-05-09 17:42:45 +0200

    implementacja odejmowanka

--- src/MathOper.java
+++ src/MathOper.java
@@ -1,6 +1,11 @@
-public class MathOper implements IArithemicsAdd {
+public class MathOper implements IArithemicsAdd, IArithmeticsDiff {
     @Override
     public double Addition(double A, double B) {
         return A + B;
+    }
+
+    @Override
+    public double Difference(double A, double B) {
+        return A - B;
     }
 }
