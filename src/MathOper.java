2638cb75d882b8514425a17e1701f396179aac74
rwitczyk <r.witczyk@wp.pl>
2019-05-16 12:57:05 +0200

    zad7
    Zad4 Zaimplementowanie dzielenia
    
    (cherry picked from commit 8dade47911778e925234c8230351c2ca21f6a521)

--- src/MathOper.java
+++ src/MathOper.java
@@ -1,6 +1,11 @@
-public class MathOper implements IArithemicsAdd {
+public class MathOper implements IArithemicsAdd, IArithmeticsDiv {
     @Override
     public double Addition(double A, double B) {
         return A + B;
+    }
+
+    @Override
+    public double Devision(double A, double B) {
+        return A / B;
     }
 }
