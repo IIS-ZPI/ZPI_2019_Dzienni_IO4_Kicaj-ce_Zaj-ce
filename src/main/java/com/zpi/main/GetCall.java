81b427a408c0ab580c41646032c109abfb1bd033
2089278 <2089278@edu.p.lodz.pl>
2019-06-06 16:04:37 +0200

    refactor

--- src/main/java/com/zpi/main/GetCall.java
+++ src/main/java/com/zpi/main/GetCall.java
@@ -6,6 +6,6 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface GetCall {
-    @GET("rates/a/gbp/last/{days}/")
-    Call<Quotations> loadRatesForSpecyfiedNumberOfDays(@Path("days") String numberOfDays, @Query("format") String format);
+    @GET("rates/a/{currency}/last/{days}/")
+    Call<Quotations> loadRatesForSpecyfiedNumberOfDays(@Path("currency") String currency, @Path("days") String numberOfDays, @Query("format") String format);
 }
