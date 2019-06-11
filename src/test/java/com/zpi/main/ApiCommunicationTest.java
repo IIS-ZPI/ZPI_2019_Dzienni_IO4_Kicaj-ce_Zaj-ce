de389ae190884f80bc2fed379c25b9d830a6d12b
209420 <209420@edu.p.lodz.pl>
2019-06-11 10:57:37 +0200

    when_Given_Specified_Days_Should_Return_Specified_Values

--- src/test/java/com/zpi/main/ApiCommunicationTest.java
+++ src/test/java/com/zpi/main/ApiCommunicationTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -35,12 +36,19 @@ public class ApiCommunicationTest {
     @Test
     public void when_Given_Specified_Days_Should_Return_Specified_Values() throws InterruptedException {
 
-       /* String fromDate="2019-06-06";
+        String fromDate="2019-06-06";
         String toDate="2019-06-11";
-        //String result =apiCommunication.getSessions(fromDate, toDate, "gbp").getRates();
-        String expectedResult="GBP\n4.8274\n4.827\n4.7897\n";
-        Assert.assertEquals(expectedResult,result);
-*/
+        List<Rate> result =apiCommunication.getSessions(fromDate, toDate, "gbp").getRates();
+        List<Double> expectedResult= new ArrayList<Double>();
+        expectedResult.add(4.8274);
+        expectedResult.add(4.8270);
+        expectedResult.add(4.7897);
+        for(int i=0;i<3;i++){
+            Assert.assertEquals(expectedResult.get(i).doubleValue(), result.get(i).getMid(),0.001);
+
+        }
+
+
     }
 
 
